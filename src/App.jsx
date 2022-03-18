7add592dc17546e3a3f7e2a21ef1381c527a8982
Atai <atai.ibraimov10@gmail.com>
2022-03-18 11:33:39 +0600

    finished

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./header";
+import Footer from "./Footer";
+
+const App = () => {
+    return (
+        <div>
+            <Header/>
+            <h1>Lorem ipsum dolor sit amet.</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis, reprehenderit.</p>
+            <Footer/>
+        </div>
+    )
+}
+export default App;
